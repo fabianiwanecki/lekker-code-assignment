9e3090bec23e97ba437f8ddc8c410bc597fdd928
Fabian Iwanecki <fabian@iwanecki.de>
2024-07-05 16:25:05 +0200

    initial project setup

---
+++ Game Monitoring Application/src/main/java/com/iwanecki/gamemonitoring/GameMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.iwanecki.gamemonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameMonitoringApplication.class, args);
+    }
+
+}
---
+++ Game Monitoring Application/src/test/java/com/iwanecki/gamemonitoring/GameMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iwanecki.gamemonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
